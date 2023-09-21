149ca1a9cdc06d27f45bacb07611b3b7b7088667
kieran <kieran-mcdonagh@outlook.com>
2023-09-21 15:28:32 +0100

    adds groups api

--- data/runSeed.js
+++ data/runSeed.js
@@ -1,11 +1,12 @@
-const devData = require("../data/devData/users");
+const users = require("../data/devData/users");
+const groups = require("../data/devData/groups")
 const { seed } = require("../data/seed");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const runSeed = async () => {
   await mongoose.connect(process.env.DATABASE_URL);
-  return seed(devData).then(() => mongoose.connection.close());
+  return seed(users, groups).then(() => mongoose.connection.close());
 };
 
 runSeed();
